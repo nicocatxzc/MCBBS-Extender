bdac516eda6b9781f01d9ca6501e5a0801cf5d51
nicocatxzc <nicocatxzc@163.com>
2026-06-03 00:32:46 +0800

    更新版本号至2.2.8

--- src/meta.js
+++ src/meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MCBBS Extender
 // @namespace    https://nicocat.cc
-// @version      release-2.2.7
+// @version      release-2.2.8
 // @description  MCBBS行为拓展/样式修复
 // @author       nicocat
 // @icon         https://www.mcbbs.co/favicon.ico
