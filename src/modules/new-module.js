cb186539163c6ae06bd04a7c85aac0832fbbfaf6
nicocatxzc <nicocatxzc@163.com>
2026-07-10 13:23:42 +0800

    feat:新增模块模板代码

---
+++ src/modules/new-module.js
@@ -0,0 +1,33 @@
+// Module: 通用模块模板
+(() => {
+    let MExt = unsafeWindow.MExt;
+    let $ = MExt.jQuery;
+    let dlg = MExt.debugLog;
+    let Stg = MExt.ValueStorage;
+    let module = {
+        runcase: () => {
+            return MExt.ValueStorage.get("module");
+        },
+        config: [
+            {
+                id: "Module",
+                default: false,
+                type: "check",
+                name: "Module",
+                desc: "",
+            },
+        ],
+        style: /* css */ `
+// css
+`,
+        core: () => {
+            // start
+            dlg("Module enabled。");
+
+            $(() => {
+                // run case
+            });
+        },
+    };
+    MExt.exportModule(module);
+})();
